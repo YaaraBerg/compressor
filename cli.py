f2f0f739265a74e622f0fffbe99600b088a80bc7
YaaraBerg <102763378+YaaraBerg@users.noreply.github.com>
2026-01-04 23:37:16 +0200

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,10 +1,8 @@
-"""
-Main CLI entry point
-"""
 import sys
 from bitarray import bitarray
 from bitarray.util import ba2int, int2ba
 from huffman_coding import huffman_encode, huffman_decode
+from dynamic_hashed_lampel_ziv import dynamic_lempel_ziv_encoder, dynamic_lempel_ziv_decoder
 
 
 def main():
@@ -26,8 +24,13 @@ def main():
             enc_huff16 = huffman_encode(data, symbol_bits=16)
         except:
             enc_huff16 = None
+        try:
+            enc_lampel_ziv = dynamic_lempel_ziv_encoder(data, search_length=2048, match_length=2048,
+                                                        minimum_match_length=3)
+        except:
+            enc_lampel_ziv = None
         enc_plain = data
-        options = [enc_huff8, enc_huff16, enc_plain]
+        options = [enc_huff8, enc_huff16, enc_lampel_ziv, enc_plain]
         best_index = min(range(3), key=lambda i: len(options[i]) if options[i] is not None else float('inf'))
         # index - 2 bits
         data_with_header = int2ba(best_index, 2) + options[best_index]
@@ -41,6 +44,9 @@ def main():
         elif header == 1:
             decoded = huffman_decode(body, 16)
         elif header == 2:
+            decoded = dynamic_lempel_ziv_decoder(body, search_length=2048, match_length=2048,
+                                                minimum_match_length=3)
+        elif header == 3:
             decoded = body
         with open(result_file, 'wb') as f:
             f.write(decoded)
